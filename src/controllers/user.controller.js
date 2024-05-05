cccb2cdfd2b54b6217d939d6701a7031eed495ab
developwithhammad <hammadquiz@gmail.com>
2024-05-05 14:33:17 +0500

    user login and user logout functionality completed || authentication middleware created || function for generate tokens after login completed || clear token after logout functionality completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,9 +10,26 @@ import { ApiResponse } from "../utils/ApiReponse.js";
 // check for images, avatar ===>
 // upload images, avatar on cloudinary ===>
 // create user object or details and store in db ===>
-// remove password and refresh token field from res
-// check for user creation
-// return res
+// remove password and refresh token field from res ===>
+// check for user creation ===>
+// return res ===>
+
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, password, username, avatar, coverImage } = req.body;
@@ -68,6 +85,84 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).send(
         new ApiResponse(200, "User registered successfully", createdUser)
     )
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    // req.body ==>>>>>>>>
+    // username or email ===>
+    // find the user ==>
+    // password check ===>
+    // access and refresh token
+    // send cookie
+
+    const { username, password } = req.body;
+    console.log("username ==>", username);
+    console.log("password ==>", password);
+
+    if (!username || !password) {
+        throw new ApiError(400, "username or password must required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { password }]
+    })
+    if (!existedUser) {
+        throw new ApiError(404, "user with this username is not exist!")
+    }
+
+    const isPasswordValid = await existedUser.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(existedUser._id)
+
+    const loggedInUser = await User.findById(existedUser._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully"
+            )
+        )
+
 })
 
-export default registerUser;
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(req.user._id, {
+        $set: { refreshToken: undefined }
+    },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,7 +46,7 @@ const userSchema = new Schema({
     { timestamps: true }
 )
 
-userSchema.pre("save", async function (next){
+userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
     this.password = await bcyrpt.hash(this.password, 10)
@@ -62,7 +62,7 @@ userSchema.methods.generateAccessToken = function () {
         _id: this._id,
         email: this.email,
         username: this.username,
-        fullname: this.fullname,
+        fullName: this.fullName,
     }, process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
